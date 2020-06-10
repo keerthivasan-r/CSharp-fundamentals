fdb3b192d2bde0aa1108f24c80c172a9e79cd6e9
keerthivasan-r <keerthivasan.dev@gmail.com>
2020-06-10 11:41:08 +0530

    Add Book class

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,6 +8,7 @@ namespace GradeBook
         // pass params to application via array of strings
         static void Main(string[] args)
         {
+            var book = new Book();
             var result = 0.0;
             List<double> grades = new List<double>(){21.3,32.7};
             foreach(var number in grades){
