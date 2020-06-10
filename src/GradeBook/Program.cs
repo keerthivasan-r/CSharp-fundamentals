6b09a7ff7c19cda6f41ceb5ede069b8008e50222
keerthivasan-r <keerthivasan.dev@gmail.com>
2020-06-10 10:31:39 +0530

    implicit type 'var' and array concepts

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,11 +7,13 @@ namespace GradeBook
         // pass params to application via array of strings
         static void Main(string[] args)
         {
+            var numbers = new[] {21.3,24,1.32};
+            var result = 0.0;
+            foreach(var number in numbers){
+                result+=number;
+            }
+            Console.WriteLine(result);
            
-            if(args.Length > 0)
-            Console.WriteLine($"Hello {args[0]} !");
-            else
-            Console.WriteLine("Hello, World");
         }
     }
 }
