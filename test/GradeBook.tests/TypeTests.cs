4ac52be67e6b5ebff3cca6222be3748b972cf0c5
keerthivasan-r <keerthivasan.dev@gmail.com>
2020-06-15 17:00:20 +0530

    Add GetBookReturnsDifferentObjects test

--- test/GradeBook.tests/TypeTests.cs
+++ test/GradeBook.tests/TypeTests.cs
@@ -7,12 +7,17 @@ namespace GradeBook.tests
     public class TypeTests
     {
         [Fact]
-        public void Test1()
+        public void GetBookReturnsDifferentObjects()
         {
-            var book1 = new Book("Book 1");
-            var book2 = new Book("Book 2");
+            var book1 = GetBook("Book 1");
+            var book2 = GetBook("Book 2");
             Assert.Equal("Book 1", book1.Name);
             Assert.Equal("Book 2", book2.Name);
+        }
+
+        Book GetBook(string name)
+        {
+            return new Book(name);
         }
 
     }
